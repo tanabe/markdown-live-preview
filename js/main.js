2d8e5c904f77c1211a21b84210ddbc00881de89c
tanabe <tanablog@gmail.com>
2011-05-17 23:50:41 +0900

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
+var example = [
+  "# hello, This is Markdown Live Previewer",
+  "",
+  "----",
+  "## what is Markdown?",
+  "see [Wikipedia](http://en.wikipedia.org/wiki/Markdown)",
+  "",
+  "> Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people \"to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)\".",
+  "",
+  "----",
+  "## usage",
+  "1. write markdown text in above textarea",
+  "2. render automatically to the output area",
+  "",
+  "----",
+  "## thanks",
+  "* [markdown-js](https://github.com/evilstreak/markdown-js)"
+].join("\n");
 $(function() {
+  $("#markdown").val(example);
+  $("#output").html(markdown.toHTML(example));
+  $("#output").html(markdown.toHTML(example));
   $("#markdown").bind("keyup", function() {
     $("#output").html(markdown.toHTML($("#markdown").val()));
   });
