0ddec7c9373228d42225e1870caa2049f3336218
Hideaki Tanabe <tanablog@gmail.com>
2020-08-10 22:32:13 +0900

    Fix #16

--- public/js/main.js
+++ public/js/main.js
@@ -7,50 +7,20 @@ $(function() {
 
   var isEdited = false;
 
-  //$('#markdown').val(example);
+  let convert = () => {
+    let html = marked($('#markdown').val());
+    $('#output').html(html);
+  }
+
   $('#markdown').bind('keyup', function() {
     isEdited = true;
-    $('#output').html(markdown.toHTML($('#markdown').val()));
+    convert();
     $('#output a').each(function(index, element) {
         var href = element.getAttribute('href');
         if (RegExp('^javascript', 'i').test(href)) {
             element.setAttribute('href', '#');
         }
     });
-  });
-
-  //menu
-  var menuItems = $('#menu a');
-  menuItems.click(function(event) {
-    event.preventDefault();
-
-    menuItems.removeClass('active');
-    var sender = $(event.currentTarget);
-    sender.addClass('active');
-
-    $('#content .mode').hide();
-    var menuId = sender.data('menuId');
-    currentMode = menuId;
-    $('#' + menuId).show();
-    $(window).scrollTop(scrollTops[currentMode]);
-
-  });
-
-  //reference
-  $("table#reference tr td:odd").each(function(index, element) {
-    var self = $(element);
-    if (self.html() === "") {
-      self.html(markdown.toHTML(self.siblings().html()));
-    }
-  });
-
-  //clear
-  $('#clearButton').click(function(event) {
-    event.preventDefault();
-    if (window.confirm('Are you sure you want to delete?')) {
-      $('#markdown').val('');
-      $('#output').html('');
-    }
   });
 
   //autoresize
@@ -63,5 +33,5 @@ $(function() {
     }
   });
 
-  $('#output').html(markdown.toHTML($('#markdown').val()));
+  convert();
 });
