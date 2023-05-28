2d3357c42c49f5ca3ea7a0a8bc8fdced1e853ec4
Hideaki Tanabe <tanablog@gmail.com>
2023-05-28 23:18:50 +0900

    WIP saving changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,78 @@
 $(function() {
     let hasEdited = false;
 
+    const confirmationMessage = 'Are you sure you want to leave? Your changes will be lost.';
+
+    // default template
+    const defaultInput = `# Markdown syntax guide
+
+## Headers
+
+# This is a Heading h1
+## This is a Heading h2 
+###### This is a Heading h6
+
+## Emphasis
+
+*This text will be italic*  
+_This will also be italic_
+
+**This text will be bold**  
+__This will also be bold__
+
+_You **can** combine them_
+
+## Lists
+
+### Unordered
+
+* Item 1
+* Item 2
+* Item 2a
+* Item 2b
+
+### Ordered
+
+1. Item 1
+1. Item 2
+1. Item 3
+  1. Item 3a
+  1. Item 3b
+
+## Images
+
+![This is a alt text.](/image/sample.png "This is a sample image.")
+
+## Links
+
+You may be using [Markdown Live Preview](https://markdownlivepreview.com/).
+
+## Blockquotes
+
+> Markdown is a lightweight markup language with plain-text-formatting syntax, created in 2004 by John Gruber with Aaron Swartz.
+>
+>> Markdown is often used to format readme files, for writing messages in online discussion forums, and to create rich text using a plain text editor.
+
+## Tables
+
+| Left columns  | Right columns |
+| ------------- |:-------------:|
+| left foo      | right foo     |
+| left bar      | right bar     |
+| left baz      | right baz     |
+
+## Blocks of code
+
+${"`"}${"`"}${"`"}
+let message = 'Hello world';
+alert(message);
+${"`"}${"`"}${"`"}
+
+## Inline code
+
+This web site is using ${"`"}markedjs/marked${"`"}.
+`;
+
     let adjustScreen = () => {
         let screenHeight = $(window).height();
         let headerHeight = $('#header').outerHeight();
@@ -14,41 +86,70 @@ $(function() {
         adjustScreen();
     });
 
-    // Setup editor
-    let editor = ace.edit('editor');
-    editor.getSession().setUseWrapMode(true);
-    editor.renderer.setScrollMargin(10, 10, 10, 10);
-    editor.setOptions({
-        maxLines: Infinity,
-        indentedSoftWrap: false,
-        fontSize: 14,
-        autoScrollEditorIntoView: true,
-        theme: 'ace/theme/github',
-        // TODO consider some options
-    });
-    editor.on('change', () => {
-        hasEdited = true;
-        convert();
-        adjustScreen();
-    });
+    let setupEditor = () => {
+        let editor = ace.edit('editor');
+        editor.getSession().setUseWrapMode(true);
+        editor.renderer.setScrollMargin(10, 10, 10, 10);
+        editor.setOptions({
+            maxLines: Infinity,
+            indentedSoftWrap: false,
+            fontSize: 14,
+            autoScrollEditorIntoView: true,
+            theme: 'ace/theme/github',
+            // TODO consider some options
+        });
+        editor.on('change', () => {
+            let changed = editor.getValue() != defaultInput;
+            if (changed) {
+                hasEdited = true;
+            }
+            convert(editor.getValue());
+            adjustScreen();
+        });
+        return editor;
+    };
 
-    let convert = () => {
+    // Render markdown text as html
+    let convert = (markdown) => {
         let options = {
             headerIds: false,
             mangle: false
         };
-        let html = marked.parse(editor.getValue(), options);
+        let html = marked.parse(markdown, options);
         let sanitized = DOMPurify.sanitize(html);
         $('#output').html(sanitized);
-    }
+    };
+
+    // Reset input text
+    let reset = () => {
+        if (hasEdited) {
+            var confirmed = window.confirm(confirmationMessage);
+            if (!confirmed) {
+                return;
+            }
+        }
+        editor.setValue(defaultInput);
+        editor.moveCursorTo(0, 0);
+        editor.focus();
+        editor.navigateLineEnd();
+        hasEdited = false;
+    };
+
+    // Setup reset button
+    document.querySelector("#reset").addEventListener('click', (event) => {
+        event.preventDefault();
+        reset();
+    });
     
     // Confirms before leaving
     $(window).bind('beforeunload', function() {
       if (hasEdited) {
-        return 'Are you sure you want to leave? Your changes will be lost.';
+        return confirmationMessage;
       }
     });
 
-    convert();
+    // entry point
+    let editor = setupEditor();
+    reset();
     adjustScreen();
 });
