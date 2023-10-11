12fdd562349cb7d3d9b767bb16005c26ec798ce7
Hideaki Tanabe <tanablog@gmail.com>
2023-10-11 21:30:54 +0900

    Add sync scroll bar feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", (event) => {
+document.addEventListener("DOMContentLoaded", () => {
     let hasEdited = false;
+    let scrollBarSync = false;
 
     const localStorageNamespace = 'com.markdownlivepreview';
     const localStorageKey = 'last_state';
+    const localStorageScrollBarKey = 'scroll_bar_settings';
     const confirmationMessage = 'Are you sure you want to reset? Your changes will be lost.';
 
     // default template
@@ -36,10 +38,10 @@ _You **can** combine them_
 ### Ordered
 
 1. Item 1
-1. Item 2
-1. Item 3
-  1. Item 3a
-  1. Item 3b
+2. Item 2
+3. Item 3
+    1. Item 3a
+    2. Item 3b
 
 ## Images
 
@@ -94,9 +96,11 @@ This web site is using ${"`"}markedjs/marked${"`"}.
             if (changed) {
                 hasEdited = true;
             }
-            convert(editor.getValue());
-            saveLastState();
+            let value = editor.getValue();
+            convert(value);
+            saveLastContent(value);
         });
+
         return editor;
     };
 
@@ -134,6 +138,42 @@ This web site is using ${"`"}markedjs/marked${"`"}.
         hasEdited = false;
     };
 
+    // ----- sync scroll position -----
+
+    let initScrollBarSync = (settings) => {
+        let checkbox = document.querySelector('#sync-scroll-checkbox');
+        checkbox.checked = settings;
+        scrollBarSync = settings;
+
+        checkbox.addEventListener('change', (event) => {
+            let checked = event.currentTarget.checked;
+            scrollBarSync = checked;
+            saveScrollBarSettings(checked);
+        });
+
+        document.querySelector('#edit').addEventListener('scroll', (event) => {
+            if (!scrollBarSync) {
+                return;
+            }
+            let editorElement = event.currentTarget;
+            let ratio = editorElement.scrollTop / (editorElement.scrollHeight - editorElement.clientHeight);
+
+            let previewElement = document.querySelector('#preview');
+            let targetY = (previewElement.scrollHeight - previewElement.clientHeight) * ratio;
+            previewElement.scrollTo(0, targetY);
+        });
+    };
+
+    let enableScrollBarSync = () => {
+        scrollBarSync = true;
+    };
+
+    let disableScrollBarSync = () => {
+        scrollBarSync = false;
+    };
+
+    // ----- clipboard utils -----
+
     let copyToClipboard = (text, successHandler, errorHandler) => {
         navigator.clipboard.writeText(text).then(
             () => {
@@ -153,6 +193,8 @@ This web site is using ${"`"}markedjs/marked${"`"}.
             labelElement.innerHTML = "Copy";
         }, 1000)
     };
+
+    // ----- setup -----
 
     // setup navigation actions
     let setupResetButton = () => {
@@ -175,25 +217,41 @@ This web site is using ${"`"}markedjs/marked${"`"}.
         });
     };
 
+    // ----- local state -----
 
-    let loadLastState = () => {
-        let lastState = Storehouse.getItem(localStorageNamespace, localStorageKey);
-        return lastState;
+    let loadLastContent = () => {
+        let lastContent = Storehouse.getItem(localStorageNamespace, localStorageKey);
+        return lastContent;
     };
 
-    let saveLastState = () => {
+    let saveLastContent = (content) => {
         let expiredAt = new Date(2099, 1, 1);
-        Storehouse.setItem(localStorageNamespace, localStorageKey, editor.getValue(), expiredAt);
+        Storehouse.setItem(localStorageNamespace, localStorageKey, content, expiredAt);
     };
 
-    // entry point
-    let lastState = loadLastState();
+    let loadScrollBarSettings = () => {
+        let lastContent = Storehouse.getItem(localStorageNamespace, localStorageScrollBarKey);
+        return lastContent;
+    };
+
+    let saveScrollBarSettings = (settings) => {
+        let expiredAt = new Date(2099, 1, 1);
+        Storehouse.setItem(localStorageNamespace, localStorageScrollBarKey, settings, expiredAt);
+    };
+
+
+    // ----- entry point -----
+
+    let lastContent = loadLastContent();
     let editor = setupEditor();
-    if (lastState) {
-        presetValue(lastState);
+    if (lastContent) {
+        presetValue(lastContent);
     } else {
         presetValue(defaultInput);
     }
     setupResetButton();
     setupCopyButton(editor);
+
+    let scrollBarSettings = loadScrollBarSettings() || false;
+    initScrollBarSync(scrollBarSettings);
 });
